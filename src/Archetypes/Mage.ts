24e7e29055c507c7feb73d287da876beae1eb19e
Cristiano Vieira <cristianoviieira@gmail.com>
2022-09-22 19:27:36 -0300

    feat: adiciona nova classe de mage para o jogo

---
+++ src/Archetypes/Mage.ts
@@ -0,0 +1,31 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Mage extends Archetype {
+  private static quantity = 0;
+  constructor(
+    name: string,
+    special: number,
+    cost: number,
+    private _energyType: EnergyType,
+  ) {
+    super(name, special, cost);
+    Mage.addNewMage();
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  private static addNewMage(): void {
+    this.quantity += 1;
+  }
+
+  static createdArchetypeInstances(): number {
+    return this.quantity;
+  }
+}
+
+const a = new Mage('maguinho', 1, 1, 'mana');
+
+console.log(a.name);
