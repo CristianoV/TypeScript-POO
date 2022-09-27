3ef3327f5e6cc1ea64859a7b5d3b24118d126648
CristianoV <cristianoviieira@gmail.com>
2022-09-27 02:05:52 -0300

    fix: atualização de tipagens desnecessarias

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -2,12 +2,8 @@ import Energy from '../Energy';
 import { SimpleFighter } from './SimpleFighter';
 
 export default interface Fighter extends SimpleFighter {
-  lifePoints: number;
-  strength: number;
   defense: number;
   energy?: Energy;
-  attack(enemy: Fighter): void;
   special(enemy?: Fighter): void;
   levelUp(): void;
-  receiveDamage(attackPoints: number): number;
 }
